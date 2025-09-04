b973ad18aebc5f0ff6c7d6574d472ff0b90a4f59
Bhavesh Bhagoria <bhaveshbhagoria@gmail.com>
2025-09-04 19:06:47 +0530

    This is my first commit

---
+++ menu_list.py
@@ -0,0 +1,17 @@
+import menu_list_function as mlf
+def select():
+	choice = int(input("Press 1 for add\nPress 2 for sub\nPress 3 for mul\nEnter a valid choice : "))
+	if choice == 1 :
+		result = mlf.addition()
+		print(result)
+	elif choice == 2 :
+		result = mlf.substraction()
+		print(result)
+	elif choice == 3 :
+		result = mlf.multiplication()
+		print(result) 	
+	else : 
+		print("Invalid Choice")
+		select()
+
+select()
---
+++ menu_list_function.py
@@ -0,0 +1,17 @@
+def addition():
+	opr1 = int(input("Enter Operand 1 : "))
+	opr2 = int(input("Enter Operand 2 : "))
+	return opr1 + opr2 
+def substraction():
+	opr1 = int(input("Enter Operand 1 : "))
+	opr2 = int(input("Enter Operand 2 : "))
+	return opr1 - opr2 
+def multiplication():
+	opr1 = int(input("Enter Operand 1 : "))
+	opr2 = int(input("Enter Operand 2 : "))
+	return opr1 * opr2 
+def division():
+	opr1 = int(input("Enter Operand 1 : "))
+	opr2 = int(input("Enter Operand 2 : "))
+	return opr1 / opr2 
+
