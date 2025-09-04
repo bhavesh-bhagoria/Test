d4e3beace13bed159dec78077b26f83d5fa52da9
Bhavesh Bhagoria <bhaveshbhagoria@gmail.com>
2025-09-04 19:11:08 +0530

    Change Reflection sample

--- menu_list.py
+++ menu_list.py
@@ -1,4 +1,5 @@
 import menu_list_function as mlf
+#this is the sample change 
 def select():
 	choice = int(input("Press 1 for add\nPress 2 for sub\nPress 3 for mul\nEnter a valid choice : "))
 	if choice == 1 :
