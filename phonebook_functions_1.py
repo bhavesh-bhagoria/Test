d9eb729947f722038e4b7258a725d7c3f1fed095
Bhavesh Bhagoria <bhaveshbhagoria@gmail.com>
2025-09-17 14:52:57 +0530

    changes done

--- phonebook_functions_1.py
+++ phonebook_functions_1.py
@@ -11,66 +11,48 @@ def create_contact():
 	file.write("{},{},{}\n".format(name,email,phone_number))
 	file.close()
 
+def readfile():
+	file = open(record_file,"r")
+	contact_records = file.readlines()
+	file.close()
+	return contact_records
+
+def display_record(contact_item) :
+	print("=============================")
+	print("Name  : ",contact_item[0])
+	print("Email : ",contact_item[1])
+	print("Phone : ",contact_item[2])
+	print("=============================")
+
 def display_all_contact():
-	file = open(record_file,"r")
-	contact_records = file.readlines()
-	#print(contact_records)
-	file.close()
-
+	contact_records = readfile()
 	for contact in contact_records : 
-		#print(contact)
 		contact = contact.replace("\n","")
-		#print(contact)
 		contact_item = contact.split(",")
-		#print(contact_item)
-		print("=============================")
-		print("Name  : ",contact_item[0])
-		print("Email : ",contact_item[1])
-		print("Phone : ",contact_item[2])
-		print("=============================")
-
-		
+		display_record(contact_item)
+	
 def search_contact():
-	file = open(record_file,"r")
-	contact_records = file.readlines()
-	#print(contact_records)
-	file.close()
-
+	contact_records = readfile()
 	search_term = input("Enter a valid Search Term : ")
+
 	for contact in contact_records : 
 		contact = contact.replace("\n","")
 		contact_item = contact.split(",")
 		if search_term in contact_item : 
-			print("=============================")
-			print("Name  : ",contact_item[0])
-			print("Email : ",contact_item[1])
-			print("Phone : ",contact_item[2])
-			print("=============================")
+			display_record(contact_item)
+			return contact_records,contact_item
 			break
 	else : 
 		print("Contact not found")
 
+def write_records(contact_records) :
+	file = open(record_file,"w")
+	for i in contact_records :
+		file.write(i)
+	file.close()
 
 def update_contact():
-	file = open(record_file,"r")
-	contact_records = file.readlines()
-	file.close()
-
-	search_term = input("Enter a valid Search Term : ")
-	for contact in contact_records : 
-		contact = contact.replace("\n","")
-		contact_item = contact.split(",")
-		if search_term in contact_item : 
-			print("=============================")
-			print("Name  : ",contact_item[0])
-			print("Email : ",contact_item[1])
-			print("Phone : ",contact_item[2])
-			print("=============================")
-			current_contact = contact_item	
-			break
-	else : 
-		print("Contact not found")
-
+	contact_records,current_contact = search_contact()
 	update_contact = current_contact
 	current_contact_entry = "{},{},{}\n".format(current_contact[0],current_contact[1],current_contact[2])
 	change_choice = int(input("Press 1 to Change Name\nPress 2 to Change Email\nPress 3 to change Phone\nEnter valid change choice : "))
@@ -84,22 +66,26 @@ def update_contact():
 		print("Invalid choice")
 
 	update_contact_entry = "{},{},{}\n".format(update_contact[0],update_contact[1],update_contact[2])
-
-	#print("Before Change")
-	#print(contact_records)
-
 	change_index = contact_records.index(current_contact_entry)
 	contact_records[change_index] = update_contact_entry
 
-	#print("After Change")
-	#print(contact_records)
-	file = open(record_file,"w")
-	for i in contact_records :
-		file.write(i)
-	file.close()
-
-
+	write_records(contact_records)
 	print("Record Updated Successfully")
-			
+
+
 def delete_contact():
-	pass
+	contact_records = readfile()
+
+	search_term = input("Enter a valid Search Term: ")
+	for contact in contact_records:
+		contact_item = contact.replace("\n", "").split(",")
+		if search_term in contact_item:
+			display_record(contact_item)
+			current_contact_entry = "{},{},{}\n".format(contact_item[0], contact_item[1], contact_item[2])
+			break
+		else:
+			print("Contact not found")
+		return
+	contact_records.remove(current_contact_entry)
+	records = write_records(contact_records)
+	print("Record Deleted Successfully")
